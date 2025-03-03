21321067402a67e48d0f5c06dde5c4209ff8f817
ruslayni <ki22.r-yefimkin@nubip.edu.ua>
2025-03-03 23:55:13 +0200

    spring time

--- script.js
+++ script.js
@@ -267,7 +267,7 @@ function createFallingHearts() {
 	setInterval(() => {
 		const heart = document.createElement('div')
 		heart.classList.add('heart')
-		heart.innerHTML = '❤️'
+		heart.innerHTML = '🌸'
 		heart.style.left = Math.random() * 100 + 'vw'
 		heart.style.animationDuration = Math.random() * 3 + 2 + 's' // От 2 до 5 секунд
 		document.getElementById('heartsContainer').appendChild(heart)
